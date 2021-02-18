c80732b1fd219f4e7a7a2e06265ab95a2a4c6579
evrislav <evrislav@gmail.com>
2021-02-18 23:14:15 +0000

    Add package description

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='dinet',
       version='0.1',
       description='Python interactive console app for generating docker compose structure',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
       url='https://github.com/evrislav/dinet',
       author='Evrislav',
       author_email='evrislav@gmail.com',
