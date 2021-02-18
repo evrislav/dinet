ffe9851eae3e2f0f275170dbb3df4f62c914672c
evrislav <evrislav@gmail.com>
2021-02-18 22:18:20 +0000

    Fix package url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='dinet',
       version='0.1',
       description='Python interactive console app for generating docker compose structure',
-      url='http://github.com/evrislav/dinet',
+      url='https://github.com/evrislav/dinet',
       author='Evrislav',
       author_email='evrislav@gmail.com',
       license='MIT',
